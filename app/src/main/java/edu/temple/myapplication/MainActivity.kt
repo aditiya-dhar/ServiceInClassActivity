c45cd3119d8544cb41ddf2c479f85623576802ed
adi <tut25650@temple.edu>
2025-04-03 12:07:41 -0400

    added timer functionality to buttons

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,54 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var timerBinder : TimerService.TimerBinder? = null
+
+    val serviceConnection = object : ServiceConnection{
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            timerBinder = p1 as TimerService.TimerBinder
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            timerBinder = null
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
 
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            timerBinder?.run {
+                if (!isRunning) {
+                    start(30)
+                }
+                else {
+                    pause()
+                }
+            }
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            timerBinder?.stop()
         }
     }
 }
